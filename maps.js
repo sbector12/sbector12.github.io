e00528d2aa371fdcccbda204af968503db5435f4
sbector12 <90579018+sbector12@users.noreply.github.com>
2023-03-19 11:41:38 -0400

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,4 +1,4 @@
-let myMap = L.map("map").setView([0, 0], 2);
+let myMap = L.map("map").setView([0, 0], 1);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
