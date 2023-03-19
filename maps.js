0b273ec02c17246d50cf3028b41aedc12f4531f6
sbector12 <90579018+sbector12@users.noreply.github.com>
2023-03-19 16:10:56 -0400

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,11 +1,11 @@
-let myMap = L.map("map").setView([0, 0], 1);
+//let myMap = L.map("map").setView([0, 0], 1);
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 // Attribution is obligatory as per copyright!
   maxZoom: 20
-}).addTo(myMap);
+}).addTo(myMap)
 
 //States
-let Arkansas = L.marker([34.5574, -92.2863]).addTo(map);
+let Arkansas = L.marker([34.5574, -92.2863]).addTo(map);//
 
