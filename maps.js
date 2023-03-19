25e2a9121745c73baa1931eac53dda04ee3d5d41
sbector12 <90579018+sbector12@users.noreply.github.com>
2023-03-19 12:22:41 -0400

    Update maps.js

--- maps.js
+++ maps.js
@@ -10,3 +10,32 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
 let Colombia= L.marker([4.570868, -74.297333]).addTo(myMap);
 let Iceland = L.marker([64.963051, -19.020835]).addTo(myMap);
 
+//States
+let Arizona=L.marker([33.729759, -111.431221]).addTo(myMap);
+let Arkansas=L.marker([34.969704, -92.373123]).addTo(myMap);
+let California=
+let Colorado=
+let Connecticut=
+let Delaware=
+let DC=
+let Florida=
+let Illinois=
+let Indiana=
+let Kentucky=
+let Louisiana=
+let Maine=
+let Maryland=
+let Massachusetts=
+let Michigan=
+let Missouri=
+let Nevada=
+let NewHampshire=
+let NewJersey=
+let NewYork=
+let Ohio=
+let Pennsylvania=
+let Tennessee=
+let Texas=
+let Virginia=
+let WestVirginia=
+
