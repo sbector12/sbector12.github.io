5263af5284f0a82a5891c9606b912c5c6e743fe9
sbector12 <90579018+sbector12@users.noreply.github.com>
2023-03-23 21:05:02 -0400

    Update maps.js

--- maps.js
+++ maps.js
@@ -12,11 +12,12 @@ var Iceland=L.marker([64.9631, -19.0208]).addTo(myMap);
 var India=L.marker([20.5937, 78.9629]).addTo(myMap);
 
 //Provinces
+var Manitoba=L.marker([56.415211, -98.739075]).addTo(myMap);
 var NB=L.marker([46.498390, -66.159668]).addTo(myMap);
 var NS=L.marker([45.000000, -63.000000]).addTo(myMap);
 var Ontario=L.marker([50.000000, -85.000000]).addTo(myMap);
 var PEI=L.marker([46.250000, -63.000000]).addTo(myMap);
-var Quebec=L.marker(53.000000, -70.000000]).addTo(myMap);
+//var Quebec=L.marker(53.000000, -70.000000]).addTo(myMap);
 
 
 //States
