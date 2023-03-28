fc0dea014e1f4dc577b6bdf2bbb6bd41254019fc
sbector12 <90579018+sbector12@users.noreply.github.com>
2023-03-28 15:54:40 -0400

    Update maps.js

--- maps.js
+++ maps.js
@@ -16,8 +16,8 @@ var Manitoba=L.marker([56.415211, -98.739075]).addTo(myMap).bindPopup("Manitoba,
 var NB=L.marker([46.498390, -66.159668]).addTo(myMap).bindPopup("New Brunswick, Canada");
 var NS=L.marker([45.000000, -63.000000]).addTo(myMap).bindPopup("Nova Scotia, Canada");
 var Ontario=L.marker([50.000000, -85.000000]).addTo(myMap).bindPopup("Ontario, Canada");
-var PEI=L.marker([46.250000, -63.000000]).addTo(myMap).bindPopup("Prince Edward Island, Canada");
-var Quebec=L.marker([53, -70]).addTo(myMap).bindPopup("Quebec, Canada");
+var PEI=L.marker([46.250000, -63.000000]).addTo(myMap);
+var Quebec=L.marker([53, -70]).addTo(myMap);
 
 
 //States
