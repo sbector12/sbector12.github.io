e05acbee8cc0b58f84d074fdc2c90e0385869005
sbector12 <90579018+sbector12@users.noreply.github.com>
2023-03-31 15:31:44 -0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -4,7 +4,4 @@ var pol1Text = "This is a project I have been working on since the end of 2021. 
 pol1.addEventListener("mouseover", function(){pol1.title = pol1Text;});
 pol1.addEventListener("mouseout", function(){pol1.title = "";});
 
-var speech1 = document.getElementById("speech1");
 var speech1Text = "As a part of my local Toastmasters club, someone gives a speech talking about someone inspiring. No one volunteered to give an inspiration that meeting, so I gave a speech on the spot. A friend of mine had passed away before the meeting. I reflect on the cruel nature of this existence, and how we should use that as motivation to make the world a better place."
-pol1.addEventListener("mouseover", function(){speech1.title = speech1Text;});
-pol1.addEventListener("mouseout", function(){speech1.title = "";});
