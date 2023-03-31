c4e1d106824f0ae27971050258a846e70c82c5ea
sbector12 <90579018+sbector12@users.noreply.github.com>
2023-03-31 15:43:57 -0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -6,6 +6,6 @@ pol1.addEventListener("mouseout", function(){pol1.title = "";});
 
 
 var speech1=document.getElementById("speech1");
-var speech1Text = "As a part of my local Toastmasters club, someone gives a speech talking about someone inspiring. No one volunteered that meeting, so I gave a speech on the spot. I reflect on the cruelty of human existence and how it can be a driver to do better."
+var speech1Text = "As a part of my local Toastmasters club, someone gives a speech talking about someone inspiring. I delivered a speech on the spot. I reflect on the cruelty of human existence and how it can be a driver to make the world better."
 speech1.addEventListener("mouseover", function(){speech1.title = speech1Text;});
 speech1.addEventListener("mouseout", function(){speech1.title ="";});
