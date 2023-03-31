1079b552d25a20938510f922d59ddc4448f1d5f1
sbector12 <90579018+sbector12@users.noreply.github.com>
2023-03-31 14:49:12 -0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,4 +1,4 @@
-var pol1 = document.getElementsByClassName("pol1");
+var pol1 = document.getElementByID("pol1");
 var pol1Text = "This is a project I have been working on since the end of 2021. I am interested in finding evidence-based solutions to the big problems facing America. In each tab, you'll find policy proposals in each area of public policy."
 
 pol1.addEventListener("mouseover", function(){pol1.title = pol1Text;});
